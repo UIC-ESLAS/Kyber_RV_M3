1919abe68a2158956193df023b5b5ca28f036d97
Junhao <1561868283@qq.com>
2023-10-09 21:47:09 +0800

    update aes impl

--- RISC-V/crypto_kem/kyber768-90s/fstack/symmetric-aes.h
+++ RISC-V/crypto_kem/kyber768-90s/fstack/symmetric-aes.h
@@ -1,5 +1,5 @@
-#ifndef PQCLEAN_KYBER76890S_CLEAN_SYMMETRIC_AES_H
-#define PQCLEAN_KYBER76890S_CLEAN_SYMMETRIC_AES_H
+#ifndef SYMMETRIC_AES_H
+#define SYMMETRIC_AES_H
 
 
 
@@ -14,9 +14,9 @@ typedef struct {
     uint32_t ctr;
 } aes256xof_ctx;
 
-void PQCLEAN_KYBER76890S_CLEAN_aes256ctr_prf(uint8_t *output, size_t outlen, const uint8_t *key, uint8_t nonce);
-void PQCLEAN_KYBER76890S_CLEAN_aes256xof_absorb(aes256xof_ctx *s, const uint8_t *key, uint8_t x, uint8_t y);
-void PQCLEAN_KYBER76890S_CLEAN_aes256xof_squeezeblocks(uint8_t *out, size_t nblocks, aes256xof_ctx *s);
-void PQCLEAN_KYBER76890S_CLEAN_aes256xof_ctx_release(aes256xof_ctx *s);
+void aes256ctr_prf(uint8_t *output, size_t outlen, const uint8_t *key, uint8_t nonce);
+void aes256xof_absorb(aes256xof_ctx *s, const uint8_t *key, uint8_t x, uint8_t y);
+void aes256xof_squeezeblocks(uint8_t *out, size_t nblocks, aes256xof_ctx *s);
+void aes256xof_ctx_release(aes256xof_ctx *s);
 
 #endif
